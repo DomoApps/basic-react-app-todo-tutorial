d44d2aaa957a57dbd17a1db7557c166491adfcd7
Jonathan Guardado <jonathan.guardado@domo.com>
2024-07-17 11:46:19 -0600

    add UI components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import { TasksContainer } from "./components/TasksContainer";
 import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>Tasks Manager</h1>
+    <TasksContainer />
   </React.StrictMode>
 );
 
